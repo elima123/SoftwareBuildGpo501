d12d48230097d97618a41082e39eaa5235089b0e
elima123 <165847721+elima123@users.noreply.github.com>
2024-05-02 17:59:45 -0600

    Add files via upload

---
+++ Laboratorio19/routes/usuarios.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/usuarios.controller.js")
+const isAuth = require('../utils/is-auth.js');
+const canCreate = require('../utils/can-create.js');
+const canView = require('../utils/can-view.js');
+
+router.get('/test_json', (req, res)=>{
+    res.status(200).json({code: 200, msg:"Ok"});
+});
+router.get('/login', controller.render_login);
+router.post('/login', controller.do_login);
+router.get('/registro', controller.get_registro);
+router.post('/registro', controller.post_registro);
+router.get('/logged', isAuth, controller.get_logged);
+router.get('/obtener_usuarios', isAuth, canView, () => { });
+router.post('/obtener_usuarios', ()=>{});
+router.get('/editar_usuario', isAuth, canCreate, controller.editar_usuario);
+router.post('/buscar_usuario', ()=>{});
+router.get('/editar_usuario', ()=>{});
+router.post('/editar_usuario', ()=>{});
+router.post('/eliminar_usuario', ()=>{});
+router.get('/logout', isAuth, canCreate, controller.logout);
+
+
+module.exports = router;
