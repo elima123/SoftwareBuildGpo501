58cf6e0b3b5253505e593dc22448a0bc7e0bd776
elima123 <165847721+elima123@users.noreply.github.com>
2024-05-13 16:05:57 -0600

    feature: Laboratorio 18

---
+++ Laboratorio18/utils/database.js
@@ -0,0 +1,18 @@
+const mariadb = require("mariadb");
+
+const pool = mariadb.createPool({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database: "users_test",
+    connectionLimit:5
+});
+
+module.exports = async () => {
+    try {
+        const connection = await pool.getConnection();
+        return connection;
+    } catch (error) {
+        throw error; // Re-throw the error for proper handling
+    }
+};
